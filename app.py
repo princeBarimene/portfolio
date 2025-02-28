0b83e3051a5a66163d6d859f2169ccd79321baee
princeBarimene <prince.g.olubari@gmail.com>
2025-02-28 23:16:27 +0000

    add year in template for the footer with the datetime module

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
@@ -6,7 +7,8 @@ app.config['SECRET_KEY'] = 'your_secret_key_here'
 # Routes
 @app.route('/')
 def home():
-    return render_template('index.html')
+    today = datetime.date.today()
+    return render_template('index.html', year = today.year)
 
 @app.route('/projects')
 def projects():
